36980710936e398f555f43a6594ca708ab7e8551
Adam Grieger <griegeac@plu.edu>
2017-03-04 18:19:31 -0800

    Finished project setup

---
+++ server/src/main/java/plu/blue/reversi/server/resources/ExampleResource.java
@@ -0,0 +1,15 @@
+package plu.blue.reversi.server.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("example")
+public class ExampleResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "This is an example response!";
+    }
+}
